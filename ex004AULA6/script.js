015a1d7048b9b762d1e5af0f3a21c922ae9a7728
Matheus Andrade <matheusandradet03@gmail.com>
2021-11-05 23:19:00 -0300

    Added Document.whiterln

--- ex004AULA6/script.js
+++ ex004AULA6/script.js
@@ -1,2 +1,4 @@
 let nome = window.prompt('Qualé o seu nome?')
-document.write(` Olá <strong>${nome}</strong>, Seu nome tem ${nome.length} letras`)
+document.write(` Olá <strong>${nome}</strong>, Seu nome tem ${nome.length} letras.<br>`)
+document.writeln(`Seu nome em maiúsculos é ${nome.toUpperCase()} <br>`)
+document.writeln(`seu nome em minusculo é ${nome.toLowerCase()}`)
